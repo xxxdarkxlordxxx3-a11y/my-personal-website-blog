76803e73c8ada1ef0c5f6badd48ed2276e376331
hamza <xxxdarkxlordxxx3@gmail.com>
2026-01-02 20:33:54 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -19,10 +18,6 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      },
-      build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
       }
     };
 });
